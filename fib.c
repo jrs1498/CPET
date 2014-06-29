9e82ef57c5057c97bb3e078096c089a647556d11
jrs1498 <jsavage1498@gmail.com>
2014-06-29 18:47:30 -0400

    initial commit

---
+++ fib.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc > 2 || argc == 1 )
+	{
+		printf("Fibonacci takes one positive integer greater than two as its argument\n");
+		return EXIT_SUCCESS;
+	}
+
+	int a, b;
+	a = 0;
+	b = 1;
+	printf( "%d", b );
+	for(int i = 0; i + a <= atof(argv[1]); b = i)
+	{
+		i = a + b;
+		a = b;
+		printf( ", %d", i );
+	}
+	printf("\n");
+
+	return EXIT_SUCCESS;
+}
